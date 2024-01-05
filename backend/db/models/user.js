26393a840bab2d71f291cece0f799052c5e822be
nudibranchy <frankietown@gmail.com>
2024-01-04 16:42:03 -0800

    finish Login route

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -64,6 +64,31 @@ module.exports = (sequelize, DataTypes) => {
     return await User.scope('currentUser').findByPk(id);
   };
 
+  User.login = async function ({ credential, password }) {
+    const { Op } = require('sequelize');
+    const user = await User.scope('loginUser').findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential
+        }
+      }
+    });
+    if (user && user.validatePassword(password)) {
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+  };
+
+  User.signup = async function ({ username, email, password }) {
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+      username,
+      email,
+      hashedPassword
+    });
+    return await User.scope('currentUser').findByPk(user.id);
+  };
+
   User.associate = function (models) {
     // associations can be defined here
   };
